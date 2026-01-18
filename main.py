bd75c934e01daea49787240d0552ed796283f68d
Ferry <FerryRietveld@live.nl>
2026-01-18 20:17:19 +0200

    Added prompts for name and difficulty

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+
+
 def main():
-    print("Hello from hangman!")
+    print("""
+     _____       _   _                           
+    |  __ \\     | | | |                          
+    | |__) |   _| |_| |__   ___  _ __            
+    |  ___/ | | | __| '_ \\ / _ \\| '_ \\           
+    | |   | |_| | |_| | | | (_) | | | |          
+  _ |_|_   \\__, |\\__|_| |_|\\___/|_| |_|          
+ | |  | |   __/ |                                
+ | |__| | _|___/ __   __ _ _ __ ___   __ _ _ __  
+ |  __  |/ _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\ 
+ | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+ |_|  |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
+                      __/ |                      
+                     |___/                       \n\n"""
+
+    )
+
+    print("Enter your name to start:")
+    name = input("What is your name?\n")
+    print(f"\nHello {name}, Let's begin shall we?")
+    print("But first, I need to know if you want this to be Easy, medium or hard?")
+    
+    while True:
+        difficulty = input("What difficulty do you want? Easy, Medium or Hard?\n").lower()
+        if difficulty in ("easy", "medium", "hard"):
+            break
+        print("Invalid choice! Please enter : Easy, Medium or Hard.\n")
 
 
 if __name__ == "__main__":
